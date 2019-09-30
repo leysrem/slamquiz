59bdb81c02bc813eafca25fe7edd5b4c7cfbde90
hochdyl <hochdyl@ndlp.fr>
2019-09-30 09:16:49 +0200

    [core] update symfony 4

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,12 +8,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/", name="default")
      */
     public function index()
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/user",name="default_user")
+     */
+    public function user() {
+
+        $first_name = "LEYS";
+        $last_name = "REMI";
+        return $this->render('default/user.html.twig', [
+            'first_name'=>$first_name,
+            'last_name'=>$last_name,
         ]);
     }
 }
